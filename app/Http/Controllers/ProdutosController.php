a3cf4319e162418af37601f12e739deba0b9a636
Luis-60 <luiscarlosghfc@gmail.com>
2025-05-31 03:02:49 -0300

    CRUD

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Produto;
+use App\Models\Componentes;
 
+use App\Http\Requests\FormRequestProduto;
 class ProdutosController extends Controller
 {
     public function __construct(Produto $produto)
@@ -20,7 +22,40 @@ class ProdutosController extends Controller
         return view('pages.produtos.paginacao', compact('findProduto'));
     }
     public function delete(Request $request)
+    {   
+        $id = $request->id;
+        $buscaRegistro = Produto::find($id);
+        $buscaRegistro->delete();
+        return response()->json(['success'=> true]);
+    }
+
+    public function cadastrarProduto(FormRequestProduto $request)
     {
-        
+        if($request->method() == "POST"){
+            $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
+            Produto::create($data);
+
+            return redirect()->route('produto.index');
+        }
+        return view('pages.produtos.create');
+    }
+
+    public function atualizarProduto(FormRequestProduto $request, $id)
+    {
+        if($request->method() == "PUT"){
+            $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
+            
+            $buscaRegistro = Produto::find($id);
+            $buscaRegistro->update($data);
+
+            return redirect()->route('produto.index');
+        }
+
+        $findProduto = Produto::where('id', '=', $id)->first();
+        return view('pages.produtos.atualiza', compact('findProduto'));
     }
 }
---
+++ app/Http/Requests/FormRequestProduto.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FormRequestProduto extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $request = [];
+        if ($this->method() =="POST" || $this->method() == "PUT"){
+            return [
+                'nome' => 'required',
+                'valor' => 'required'
+            ];
+
+        }
+        return $request;
+    }
+}
